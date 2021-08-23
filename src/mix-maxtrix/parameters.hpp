08194141c9badfe3f00598cbd3a553a356b9a319
Rafael Gago <rafael_gago_81@hotmail.com>
2021-08-23 20:32:14 +0200

    saturation: Change some labels

--- src/mix-maxtrix/parameters.hpp
+++ src/mix-maxtrix/parameters.hpp
@@ -5090,7 +5090,7 @@ parameter_cpp_class_define (
   saturation_band_gain,
   n_stereo_busses,
   param_common (
-    "Band dB",
+    "Sat Band",
     declptr<updownsampled<saturation>>(),
     declptr<saturation::saturated_out_tag>()),
   saturation::get_parameter (saturation::saturated_out_tag {}),
@@ -5100,7 +5100,7 @@ parameter_cpp_class_define (
   saturation_trim,
   n_stereo_busses,
   param_common (
-    "Trim",
+    "In Trim",
     declptr<updownsampled<saturation>>(),
     declptr<saturation::trim_tag>()),
   saturation::get_parameter (saturation::trim_tag {}),
@@ -5318,7 +5318,7 @@ parameter_cpp_class_define (
 
 using saturation_params = mp_list<
   saturation_type,
-  saturation_trim,
+  saturation_band_gain,
   saturation_drive,
   saturation_drive_balance,
   saturation_feedback,
@@ -5339,7 +5339,7 @@ using saturation_params = mp_list<
   saturation_band_hi,
   saturation_lo_mode,
   saturation_hi_mode,
-  saturation_band_gain,
+  saturation_trim,
   saturation_mode,
   saturation_oversampling>;
 //------------------------------------------------------------------------------
