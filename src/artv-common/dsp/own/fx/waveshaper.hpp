01bfddf35a2fe736b558fc0ed9994b005051706a
Rafael Gago <rafael_gago_81@hotmail.com>
2021-12-29 14:11:19 +0100

    waveshaper: Default to "exp" envelope follower

--- src/artv-common/dsp/own/fx/waveshaper.hpp
+++ src/artv-common/dsp/own/fx/waveshaper.hpp
@@ -355,7 +355,7 @@ public:
   static constexpr auto get_parameter (envfollow_mode_tag)
   {
     return choice_param (
-      0, make_cstr_array ("sqrt(exp)", "exp", "exp^2", "exp^3"), 20);
+      1, make_cstr_array ("sqrt(exp)", "exp", "exp^2", "exp^3"), 20);
   }
   //----------------------------------------------------------------------------
   struct envfollow_release_tag {};
