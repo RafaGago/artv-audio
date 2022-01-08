1472aabb9e4a74f536ef31ba1f74ed29622ce6d8
Rafael Gago <rafael_gago_81@hotmail.com>
2022-01-08 13:14:44 +0100

    biquad: Add Martin Vicanek's matched formulas

--- src/artv-common/dsp/own/parts/filters/biquad.hpp
+++ src/artv-common/dsp/own/parts/filters/biquad.hpp
@@ -10,14 +10,10 @@
 
 namespace artv {
 
-// A TDF2 biquad. For audio use the trapezoidal SVF instead. The goal of this
-// class is to be able to extract poles and zeros from the coefficients.
-//
-// Coefficient calculation from RBJ's cookbook.
-// TODO: use M.Vicaneks matched biquads:
-// https://www.vicanek.de/articles/BiquadFits.pdf
 //------------------------------------------------------------------------------
-
+// A TDF2 biquad. For audio use the trapezoidal SVF instead. The goal of this
+// class is mostly to be able to extract poles and zeros from the coefficients.
+//------------------------------------------------------------------------------
 struct biquad {
   //----------------------------------------------------------------------------
   enum coeffs { b0, b1, b2, a1, a2, n_coeffs };
@@ -25,7 +21,9 @@ struct biquad {
   enum states { z1, z2, n_states };
 
   using rev_zeros_tag = part_tick_tag<0>;
-
+  //----------------------------------------------------------------------------
+  // RBJ
+  //----------------------------------------------------------------------------
   template <class T>
   struct rbj_tag {};
 
@@ -39,6 +37,128 @@ struct biquad {
   using rbj_lowshelf_tag  = rbj_tag<lowshelf_tag>;
   using rbj_highshelf_tag = rbj_tag<highshelf_tag>;
 
+  template <class T>
+  struct mvic_hq_tag {};
+
+  using mvic_lowpass_hq_tag  = mvic_hq_tag<lowpass_tag>;
+  using mvic_highpass_hq_tag = mvic_hq_tag<highpass_tag>;
+  using mvic_bandpass_hq_tag = mvic_hq_tag<bandpass_tag>;
+  using mvic_bell_hq_tag     = mvic_hq_tag<bell_tag>;
+
+  template <class T>
+  struct mvic_tag {};
+
+  using mvic_lowpass_tag  = mvic_tag<lowpass_tag>;
+  using mvic_highpass_tag = mvic_tag<highpass_tag>;
+  using mvic_bandpass_tag = mvic_tag<bandpass_tag>;
+
+  //----------------------------------------------------------------------------
+  // Defaults
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    lowpass_tag)
+  {
+    reset_coeffs<V> (co, freq, q, sr, rbj_lowpass_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    highpass_tag)
+  {
+    reset_coeffs<V> (co, freq, q, sr, rbj_highpass_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    bandpass_tag)
+  {
+    reset_coeffs<V> (co, freq, q, sr, rbj_bandpass_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    notch_tag)
+  {
+    reset_coeffs<V> (co, freq, q, sr, rbj_notch_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    peak_tag)
+  {
+    reset_coeffs<V> (co, freq, q, sr, rbj_peak_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    allpass_tag)
+  {
+    reset_coeffs<V> (co, freq, q, sr, rbj_allpass_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    V                   gain_db,
+    vec_value_type_t<V> sr,
+    bell_tag)
+  {
+    reset_coeffs<V> (co, freq, q, gain_db, sr, rbj_bell_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    V                   gain_db,
+    vec_value_type_t<V> sr,
+    lowshelf_tag)
+  {
+    reset_coeffs<V> (co, freq, q, gain_db, sr, rbj_lowshelf_tag {});
+  }
+  //----------------------------------------------------------------------------
+  template <class V>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    V                   gain_db,
+    vec_value_type_t<V> sr,
+    highshelf_tag)
+  {
+    reset_coeffs<V> (co, freq, q, gain_db, sr, rbj_highshelf_tag {});
+  }
+  //----------------------------------------------------------------------------
+  // Robert Bristow Jhonson's
+  // https://webaudio.github.io/Audio-EQ-Cookbook/audio-eq-cookbook.html
   //----------------------------------------------------------------------------
   template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
   static void reset_coeffs (
@@ -48,6 +168,7 @@ struct biquad {
     vec_value_type_t<V> sr,
     rbj_lowpass_tag)
   {
+    // RBJ's
     using T = vec_value_type_t<V>;
 
     assert (co.size() >= n_coeffs);
@@ -73,6 +194,7 @@ struct biquad {
     vec_value_type_t<V> sr,
     rbj_highpass_tag)
   {
+    // RBJ's
     using T = vec_value_type_t<V>;
 
     assert (co.size() >= n_coeffs);
@@ -98,6 +220,7 @@ struct biquad {
     vec_value_type_t<V> sr,
     rbj_bandpass_tag)
   {
+    // RBJ's
     using T = vec_value_type_t<V>;
 
     assert (co.size() >= n_coeffs);
@@ -122,7 +245,7 @@ struct biquad {
     V                   freq,
     V                   q,
     vec_value_type_t<V> sr,
-    notch_tag)
+    rbj_notch_tag)
   {
     // RBJ's
     using T = vec_value_type_t<V>;
@@ -147,7 +270,7 @@ struct biquad {
     V                   freq,
     V                   q,
     vec_value_type_t<V> sr,
-    allpass_tag)
+    rbj_allpass_tag)
   {
     // RBJ's
     using T = vec_value_type_t<V>;
@@ -175,6 +298,7 @@ struct biquad {
     vec_value_type_t<V> sr,
     rbj_bell_tag)
   {
+    // RBJ's
     using T = vec_value_type_t<V>;
 
     assert (co.size() >= n_coeffs);
@@ -199,27 +323,11 @@ struct biquad {
     crange<V>           co,
     V                   freq,
     V                   q,
-    vec_value_type_t<V> sr,
-    bell_tag)
-  {
-    // https://www.vicanek.de/articles/BiquadFits.pdf
-    using T = vec_value_type_t<V>;
-
-    assert (co.size() >= n_coeffs);
-
-    V w0 = (T) 2. * (T) M_PI * freq / sr;
-    get_impulse_invariance_poles (co[a1], co[a2], w0, q);
-  }
-  //----------------------------------------------------------------------------
-  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
-  static void reset_coeffs (
-    crange<V>           co,
-    V                   freq,
-    V                   q,
     V                   gain_db,
     vec_value_type_t<V> sr,
     rbj_lowshelf_tag)
   {
+    // RBJ's
     using T = vec_value_type_t<V>;
 
     assert (co.size() >= n_coeffs);
@@ -249,6 +357,7 @@ struct biquad {
     vec_value_type_t<V> sr,
     rbj_highshelf_tag)
   {
+    // RBJ's
     using T = vec_value_type_t<V>;
 
     assert (co.size() >= n_coeffs);
@@ -268,6 +377,215 @@ struct biquad {
     co[a1] = (T) 2. * ((A - (T) 1) - (A + (T) 1) * cosw0) * norm;
     co[a2] = ((A + (T) 1) - (A - (T) 1) * cosw0 - g) * norm;
   }
+  //----------------------------------------------------------------------------
+  // Martin Vicanek's loose fit versions
+  // https://www.mvic.de/articles/BiquadFits.pdf
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    mvic_lowpass_tag)
+  {
+    using T = vec_value_type_t<V>;
+
+    assert (co.size() >= n_coeffs);
+
+    V f0 = (T) 2. * freq / sr; // normalized freq 0-1
+    get_impulse_invariance_poles (co[a1], co[a2], f0, q);
+
+    V r0    = (T) 1.0 + co[a1] + co[a2];
+    V f02   = f0 * f0;
+    V omf02 = (T) 1.0 - f02;
+    V num   = ((T) 1.0 - co[a1] + co[a2]) * f02;
+    V r1    = num / vec_sqrt (omf02 * omf02 + f02 / (q * q));
+
+    co[b0] = (r0 + r1) * (T) 0.5;
+    co[b1] = r0 - co[b0];
+    co[b2] = vec_set<V> (0.0);
+  }
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    mvic_highpass_tag)
+  {
+
+    using T = vec_value_type_t<V>;
+
+    assert (co.size() >= n_coeffs);
+
+    V f0 = (T) 2. * freq / sr; // normalized freq 0-1
+    get_impulse_invariance_poles (co[a1], co[a2], f0, q);
+
+    V f02   = f0 * f0;
+    V omf02 = (T) 1.0 - f02;
+    V num   = (T) 1.0 - co[a1] + co[a2];
+    V r1    = num / vec_sqrt (omf02 * omf02 + f02 / (q * q));
+
+    co[b0] = r1 * (T) 0.25;
+    co[b1] = (T) -2 * co[b0];
+    co[b2] = co[b0];
+  }
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    mvic_bandpass_tag)
+  {
+    // At first sight, this one seems to be breaking at low Q.
+    using T = vec_value_type_t<V>;
+
+    assert (co.size() >= n_coeffs);
+
+    V f0 = (T) 2. * freq / sr; // normalized freq 0-1
+    get_impulse_invariance_poles (co[a1], co[a2], f0, q);
+
+    V r0 = ((T) 1.0 + co[a1] + co[a2]) / ((T) M_PI * f0 * q);
+
+    V inv_q = (T) 1. / q;
+    V f02   = f0 * f0;
+    V omf02 = (T) 1.0 - f02;
+    V num   = ((T) 1.0 - co[a1] + co[a2]) * f0 * inv_q;
+    V r1    = num / vec_sqrt (omf02 * omf02 + f02 * inv_q * inv_q);
+
+    co[b0] = -r1 * (T) 0.5;
+    co[b1] = (r0 - co[b1]) * (T) 0.5;
+    co[b2] = -co[b0] - co[b1];
+  }
+  //----------------------------------------------------------------------------
+  // Martin Vicanek's tight fit versions
+  // https://www.mvic.de/articles/BiquadFits.pdf
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    mvic_lowpass_hq_tag)
+  {
+    using T = vec_value_type_t<V>;
+
+    assert (co.size() >= n_coeffs);
+
+    V f0 = (T) 2. * freq / sr; // normalized freq 0-1
+    get_impulse_invariance_poles (co[a1], co[a2], f0, q);
+    auto m = get_mvic_matched (co[a1], co[a2], f0 * (T) M_PI);
+
+    V R1 = (m.A0 * m.phi0 + m.A1 * m.phi1 + m.A2 * m.phi2) * (q * q);
+    V B0 = m.A0;
+    V B1 = (R1 - B0 * m.phi0) / m.phi1;
+    // according to the paper the sqrt on B1 requires double precision
+    static_assert (sizeof (T) >= sizeof (double), "Not enough precision.");
+
+    co[b0] = (T) 0.5 * (m.A0_sqrt + vec_sqrt (B1));
+    co[b1] = m.A0_sqrt - co[b0];
+    co[b2] = vec_set<V> (0.0);
+  }
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    mvic_highpass_hq_tag)
+  {
+    using T = vec_value_type_t<V>;
+
+    assert (co.size() >= n_coeffs);
+
+    V f0 = (T) 2. * freq / sr; // normalized freq 0-1
+    get_impulse_invariance_poles (co[a1], co[a2], f0, q);
+    auto m = get_mvic_matched (co[a1], co[a2], f0 * (T) M_PI);
+
+    // according to the paper the sqrt on B1 requires double precision
+    static_assert (sizeof (T) >= sizeof (double), "Not enough precision.");
+
+    V num  = vec_sqrt (m.A0 * m.phi0 + m.A1 * m.phi1 + m.A2 * m.phi2) * q;
+    co[b0] = num / ((T) 4 * m.phi1);
+    co[b1] = (T) -2 * co[b0];
+    co[b2] = co[b0];
+  }
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    vec_value_type_t<V> sr,
+    mvic_bandpass_hq_tag)
+  {
+    using T = vec_value_type_t<V>;
+
+    assert (co.size() >= n_coeffs);
+
+    V f0 = (T) 2. * freq / sr; // normalized freq 0-1
+    get_impulse_invariance_poles (co[a1], co[a2], f0, q);
+    auto m = get_mvic_matched (co[a1], co[a2], f0 * (T) M_PI);
+
+    V R1 = m.A0 * m.phi0 + m.A1 * m.phi1 + m.A2 * m.phi2;
+    V R2 = -m.A0 + m.A1 + (T) 4 * (m.phi0 - m.phi1) * m.A2;
+
+    V B2 = (R1 - R2 * m.phi1) / ((T) 4 * m.phi1 * m.phi1);
+    V B1 = R2 + (T) 4 * (m.phi1 - m.phi0) * B2;
+
+    // according to the paper some of the sqrts on B1 require double precision
+    static_assert (sizeof (T) >= sizeof (double), "Not enough precision.");
+
+    co[b1] = (T) -0.5 * vec_sqrt (B1);
+    co[b0] = (T) 0.5 * (vec_sqrt (B2 + co[b1] * co[b1]) - co[b1]);
+    co[b2] = -co[b0] - co[b1];
+  }
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void reset_coeffs (
+    crange<V>           co,
+    V                   freq,
+    V                   q,
+    V                   gain_db,
+    vec_value_type_t<V> sr,
+    mvic_bell_hq_tag)
+  {
+    using T = vec_value_type_t<V>;
+
+    assert (co.size() >= n_coeffs);
+
+    V f0 = ((T) 2. * freq) / sr; // normalized freq 0-1
+    get_impulse_invariance_poles (co[a1], co[a2], f0, q);
+    auto m = get_mvic_matched (co[a1], co[a2], f0 * (T) M_PI);
+
+    V GG = vec_exp (gain_db * (T) (1. / 20.) * (T) M_LN10);
+    GG *= GG;
+
+    V R1 = (m.A0 * m.phi0 + m.A1 * m.phi1 + m.A2 * m.phi2) * GG;
+    V R2 = (-m.A0 + m.A1 + (T) 4 * (m.phi0 - m.phi1) * m.A2) * GG;
+
+    V B0 = m.A0;
+    V B2 = (R1 - R2 * m.phi1 - B0) / ((T) 4 * m.phi1 * m.phi1);
+    V B1 = R2 + B0 + (T) 4 * (m.phi1 - m.phi0) * B2;
+
+    // according to the paper some of the sqrts on B1 require double precision
+    static_assert (sizeof (T) >= sizeof (double), "Not enough precision.");
+
+    V B0sqrt = vec_sqrt (B0);
+    V W      = (T) 0.5 * (B0sqrt + vec_sqrt (B1));
+
+    co[b0] = (T) 0.5 * (W + vec_sqrt (W * W + B2));
+    co[b1] = (T) 0.5 * (B0sqrt - vec_sqrt (B1));
+    co[b2] = -B2 / ((T) 4 * co[b0]);
+  }
+  //----------------------------------------------------------------------------
+  // processing functions
   //----------------------------------------------------------------------------
   template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
   static void reset_states (crange<V> st)
@@ -360,34 +678,69 @@ private:
     }
   }
   //----------------------------------------------------------------------------
-  template <class V>
-  static void get_impulse_invariance_poles (V& a1, V& a2, V w0, V q)
-  {
-    V exp_mq_w0 = vec_exp (-q * w0); // e^(-q w0)
-    a2          = exp_mq_w0 * exp_mq_w0; // e^(-2 q w0)
-    get_impulse_invariance_a1 (a1, w0, q, exp_mq_w0)
-  }
-  //----------------------------------------------------------------------------
-  template <class V>
-  static void get_impulse_invariance_a1 (V& a1, V w0, V q, V exp_mq_w0)
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void get_impulse_invariance_poles (V& a1, V& a2, V f0, V q)
   {
     using T = vec_value_type_t<V>;
 
-    auto cmp = (q <= vec_set<V> ((T) 1));
+    V i2q = (T) 1 / ((T) 2 * q);
+
+    V w0        = f0 * (T) M_PI; // from 0-1 to 0-PI
+    V exp_mq_w0 = vec_exp (-i2q * w0); // e^(-q w0)
+    a2          = exp_mq_w0 * exp_mq_w0; // e^(-2 q w0)
+    get_impulse_invariance_a1 (a1, w0, i2q, exp_mq_w0);
+  }
+  //----------------------------------------------------------------------------
+  template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+  static void get_impulse_invariance_a1 (V& a1, V w0, V i2q, V exp_mq_w0)
+  {
+    using T = vec_value_type_t<V>;
+
+    auto cmp = (i2q <= vec_set<V> ((T) 1));
     if (vec_is_all_ones (cmp)) {
-      return (T) -2 * exp_mq_w0 * vec_cos (vec_sqrt ((T) 1 - q * q) * w0);
+      a1 = (T) -2 * exp_mq_w0 * vec_cos (vec_sqrt ((T) 1 - i2q * i2q) * w0);
+      return;
     }
     if (vec_is_all_zeros (cmp)) {
-      return (T) -2 * exp_mq_w0 * vec_cosh (vec_sqrt (q * q - (T) 1) * w0);
+      a1 = (T) -2 * exp_mq_w0 * vec_cosh (vec_sqrt (i2q * i2q - (T) 1) * w0);
+      return;
     }
     for (uint i = 0; i < vec_traits<V>().size; ++i) {
       // do individually.
       auto a1x1 = vec_set<1> (a1[i]);
       get_impulse_invariance_a1 (
-        a1x1, vec_set<1> (w0[i]), vec_set<1> (q[i]), vec_set<1> (exp_mq_w0[i]));
+        a1x1,
+        vec_set<1> (w0[i]),
+        vec_set<1> (i2q[i]),
+        vec_set<1> (exp_mq_w0[i]));
       a1[i] = a1x1[0];
     }
   }
+  //----------------------------------------------------------------------------
+  template <class V>
+  struct mvic_matched {
+    V A0_sqrt, A0, A1, A2, phi0, phi1, phi2;
+  };
+  //----------------------------------------------------------------------------
+  template <class V>
+  static mvic_matched<V> get_mvic_matched (V a1, V a2, V w0)
+  {
+    using T = vec_value_type_t<V>;
+
+    V A0_sqrt = (T) 1.0 + a1 + a2;
+    V A0      = A0_sqrt * A0_sqrt;
+    V A1      = (T) 1.0 - a1 + a2;
+    A1 *= A1;
+    V A2 = (T) -4.0 * a2;
+
+    V phi1 = vec_sin (w0 * (T) 0.5);
+    phi1 *= phi1;
+    V phi0 = (T) 1.0 - phi1;
+    V phi2 = (T) 4.0 * phi0 * phi1;
+
+    return {A0_sqrt, A0, A1, A2, phi0, phi1, phi2};
+  }
+  //----------------------------------------------------------------------------
 };
 //------------------------------------------------------------------------------
 } // namespace artv
