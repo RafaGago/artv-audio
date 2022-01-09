ad447459eb6011d98a516ab17271dcb5be440c1e
Rafael Gago <rafael_gago_81@hotmail.com>
2022-01-09 13:54:56 +0100

    Fix TAL Reverb 2 leak

--- src/mix-maxtrix/parameters.hpp
+++ src/mix-maxtrix/parameters.hpp
@@ -6333,10 +6333,11 @@ using all_fx_typelists = mp_list<
   wonky_crossv_params,
   lin_iir_crossv_params,
   lin_eq4x_params,
-  eq4x_params>;
+  eq4x_params,
+  tal_reverb2_params>;
 
 static constexpr auto fx_choices
-  = make_cstr_array ("none", "LR", "Wonky", "lin IIR", "EQlin", "EQ4x");
+  = make_cstr_array ("none", "LR", "Wonky", "lin IIR", "EQlin", "EQ4x", "TAL");
 
 #else
 // clang-format off
