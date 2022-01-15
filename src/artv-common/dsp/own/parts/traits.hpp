950e2604d5c168991d55852bd322681e0d42f1f2
Rafael Gago <rafael_gago_81@hotmail.com>
2022-01-15 16:14:38 +0100

    traits: Add integer value to tag types

--- src/artv-common/dsp/own/parts/traits.hpp
+++ src/artv-common/dsp/own/parts/traits.hpp
@@ -1,22 +1,30 @@
 #pragma once
 
+#include "artv-common/misc/util.hpp"
+
 namespace artv {
+
+// can double as compile time and runtime tag by accessing ".value"
+template <class type, uint N>
+struct tag : public k_uint<N> {};
 
 // Tick function related tag
 struct single_coeff_set_tag {};
 
+struct filter_type_tag {};
+
 // Filter types
-struct lowpass_tag {};
-struct highpass_tag {};
-struct bandpass_tag {}; // 0dB gain independently of Q
-struct bandpass_q_gain_tag {}; // gain depends on Q
-struct notch_tag {};
-struct peak_tag {};
-struct allpass_tag {};
-struct bell_tag {}; // as in a parametric EQ
-struct bell_bandpass_tag {}; // bandpassed_bell + in = bell.
-struct lowshelf_tag {}; // as in a parametric EQ
-struct highshelf_tag {}; // as in a parametric EQ
+using lowpass_tag         = tag<filter_type_tag, 0>;
+using highpass_tag        = tag<filter_type_tag, 1>;
+using bandpass_tag        = tag<filter_type_tag, 2>; // 0dB gain peak
+using bandpass_q_gain_tag = tag<filter_type_tag, 3>; // gain depends on Q
+using notch_tag           = tag<filter_type_tag, 4>;
+using peak_tag            = tag<filter_type_tag, 5>;
+using allpass_tag         = tag<filter_type_tag, 6>;
+using bell_tag            = tag<filter_type_tag, 7>; // as in a parametric EQ
+using bell_bandpass_tag   = tag<filter_type_tag, 8>; // x + in = bell.
+using lowshelf_tag        = tag<filter_type_tag, 9>;
+using highshelf_tag       = tag<filter_type_tag, 10>;
 
 // Random tags used elsewhere.
 template <uint n>
