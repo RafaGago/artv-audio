adcda65447dd9107c7edf73db4d9bdce4ff857d7
Rafael Gago <rafael_gago_81@hotmail.com>
2022-07-21 19:50:34 +0200

    Add "add_ducker" class

---
+++ src/artv-common/dsp/own/classes/add_ducker.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <cassert>
+#include <limits>
+
+#include "artv-common/misc/misc.hpp"
+#include "artv-common/misc/range.hpp"
+#include "artv-common/misc/short_ints.hpp"
+#include "artv-common/misc/simd.hpp"
+
+#include "artv-common/dsp/own/classes/ducker.hpp"
+namespace artv {
+// ducker, for e.g. usage on reverbs/echoes
+//------------------------------------------------------------------------------
+template <class V, enable_if_vec_of_float_point_t<V>* = nullptr>
+class add_ducker {
+public:
+  //----------------------------------------------------------------------------
+  struct ducking_threshold_tag {};
+  void set (ducking_threshold_tag, float v)
+  {
+    if (v == _ducking_threshold) {
+      return;
+    }
+    _ducking_threshold = v;
+    _ducker.set_threshold (vec_set<V> (v));
+  }
+
+  static constexpr auto get_parameter (ducking_threshold_tag)
+  {
+    return float_param ("dB", -40.f, 12.f, 12.f, 0.01f);
+  }
+  //----------------------------------------------------------------------------
+  struct ducking_speed_tag {};
+  void set (ducking_speed_tag, float v)
+  {
+    if (v == _ducking_speed) {
+      return;
+    }
+    _ducking_speed = v;
+    v *= 0.01f;
+    _ducker.set_speed (vec_set<V> (v * v), (vec_value_type_t<V>) _samplerate);
+  }
+
+  static constexpr auto get_parameter (ducking_speed_tag)
+  {
+    return float_param ("%", 0.f, 100.f, 0.f, 0.01f);
+  }
+  //----------------------------------------------------------------------------
+  template <class T, class F>
+  void process (crange<T*> outs, crange<T const*> ins, uint samples, F child_f)
+  {
+    assert (outs.size() >= 2);
+    assert (ins.size() >= 2);
+
+    using VT = vec_value_type_t<V>;
+
+    constexpr uint                block_max_size = 32;
+    std::array<V, block_max_size> ducker_gain;
+    std::array<T*, 2>             out {{outs[0], outs[1]}};
+    std::array<T const*, 2>       in {{ins[0], ins[1]}};
+
+    for (uint b = 0; b < samples; b += block_max_size) {
+      uint blocksize = std::min (samples - b, block_max_size);
+
+      for (uint i = 0; i < blocksize; ++i) {
+        ducker_gain[i] = _ducker.tick (V {(VT) in[0][i], (VT) in[1][i]});
+      }
+
+      child_f (make_crange (out), make_crange (in), blocksize);
+
+      for (uint i = 0; i < blocksize; ++i) {
+        out[0][i] *= ducker_gain[i][0];
+        out[1][i] *= ducker_gain[i][1];
+      }
+
+      out[0] += block_max_size;
+      out[1] += block_max_size;
+      in[0] += block_max_size;
+      in[1] += block_max_size;
+    }
+  }
+  //----------------------------------------------------------------------------
+  void reset (float samplerate) { _samplerate = samplerate; }
+  //----------------------------------------------------------------------------
+private:
+  //----------------------------------------------------------------------------
+  ducker<V> _ducker;
+  float     _ducking_threshold = 999.f;
+  float     _ducking_speed     = 999.f;
+  float     _samplerate {};
+};
+//------------------------------------------------------------------------------
+} // namespace artv
