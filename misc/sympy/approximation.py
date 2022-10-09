8066efe46c01b66eb222bfc920ae49bc0f68b646
Rafael Gago <rafael_gago_81@hotmail.com>
2022-10-09 10:19:01 +0200

    misc: Play a bit with Sympy graphs

--- misc/sympy/approximation.py
+++ misc/sympy/approximation.py
@@ -7,7 +7,7 @@ from mpmath import mp
 
 init_printing(use_latex="mathjax")
 
-var("v")
+var("v w")
 
 mp.dps = 30 # more precision than double can handle...
 
@@ -39,5 +39,25 @@ pprint (err)
 def pan(x):
     return sin(x*1.5707963267948966)
 
+print("Pan") # Not Remez but a one liner
 chebyshev, err = mp.chebyfit(pan, [0, 1], 3, error=True)
 pprint (Poly(chebyshev, v).as_expr())
+
+print("Thiran") # Not Remez but a one liner
+def thiran1(x):
+    return 1 - (x + 0.418) / (1 + (x + 0.418))
+
+thiransp = 1 - (v + 0.418) / (1 + (v + 0.418))
+
+chebyshev, err = mp.chebyfit(thiran1, [0, 1], 5, error=True)
+pprint (Poly(chebyshev, v).as_expr())
+
+
+range = (v, 0, 1)
+p1 = plot(thiransp, range, show=False, line_color='b')
+p2 = plot (Poly(chebyshev, v).as_expr(), range, show=False, line_color='r')
+p1.extend(p2)
+p1.show()
+
+p3 = plot (thiransp - Poly(chebyshev, v).as_expr(), range, show=False, line_color='r')
+p3.show()
